9dfb1a2582f530b18dd5d305a3d73ec562a59831
STACIA <69411527+staciax@users.noreply.github.com>
2022-06-14 12:34:10 +0700

    auth fix

--- ext/valorant/auth.py
+++ ext/valorant/auth.py
@@ -17,7 +17,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 class TLSAdapter(HTTPAdapter):
     def init_poolmanager(self, *args: Any, **kwargs: Any) -> Any:
         ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        ctx.set_ciphers('ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-CHACHA20-POLY1305:ECDHE+AES128:RSA+AES128:ECDHE+AES256:RSA+AES256:ECDHE+3DES:RSA+3DES')
         kwargs['ssl_context'] = ctx
         return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
 
